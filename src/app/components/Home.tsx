fd502a342d0ae7c89ae97dff0a153b688a47cb49
Nguyen Duc Viet <vietnguyen@authenticeducationhub.com>
2024-06-27 16:29:10 +0700

    create home

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -13,34 +13,72 @@ import ImageProfile3 from "./image-profile/image3.jpg";
 import ImageProfile4 from "./image-profile/image4.jpg";
 import ImageProfile6 from "./image-profile/image6.jpg";
 import ImageProfile7 from "./image-profile/image7.jpg";
+import { Button, TextField, Typography } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import Slider from "react-slick";
+import banner1 from "./image-profile/banner1.jpg";
+import banner2 from "./image-profile/banner2.jpg";
+import banner3 from "./image-profile/banner3.jpg";
+import banner4 from "./image-profile/banner4.jpg";
+import banner6 from "./image-profile/banner6.jpg";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const HomePage = () => {
   const data = [
     {
       id: 1,
       image: ImageProfile1,
+      banner: banner2,
     },
     {
       id: 2,
       image: ImageProfile2,
+      banner: banner1,
     },
     {
       id: 3,
       image: ImageProfile3,
+      banner: banner3,
     },
     {
       id: 4,
       image: ImageProfile4,
+      banner: banner4,
     },
     {
       id: 6,
       image: ImageProfile6,
-    },
-    {
-      id: 7,
-      image: ImageProfile7,
+      banner: banner6,
     },
   ];
+
+  const settings = {
+    dots: true,
+    fade: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    waitForAnimate: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    appendDots: (dots: any) => (
+      <div
+        style={{
+          borderRadius: "10px",
+          padding: "10px",
+          position: "absolute",
+          bottom: "0",
+          left: "0",
+          right: "0",
+        }}
+      >
+        <ul style={{ margin: "0px" }}> {dots} </ul>
+      </div>
+    ),
+  };
   return (
     <div>
       <div
@@ -54,30 +92,43 @@ const HomePage = () => {
         <p className='text-[16px]'>Intern Front-End Developer in Authentic Education Hub</p>
         <div className='flex gap-3 pt-3 justify-center items-center'>
           <Link href='https://www.facebook.com/profile.php?id=100024756409898' target='_blank'>
-            <Image src={IconFacebook} alt='Facebook' width={30} height={30} />
+            <Image
+              src={IconFacebook}
+              alt='Facebook'
+              width={30}
+              height={30}
+              className='cursor-pointer'
+            />
           </Link>
 
           <Link href='https://chat.zalo.me/' target='_blank'>
-            <Image src={IconZalo} alt='' width={30} height={30} />
+            <Image src={IconZalo} alt='' width={30} height={30} className='cursor-pointer' />
           </Link>
 
           <Link href='https://www.instagram.com/vyet_oct8/' target='_blank'>
-            <Image src={IconIG} alt='' width={30} height={30} />
+            <Image
+              src={IconIG}
+              alt=''
+              width={30}
+              height={30}
+              className='cursor-pointer hover:opacity-60'
+            />
           </Link>
-          <Image src={IconTW} alt='' width={30} height={30} />
+          <Image src={IconTW} alt='' width={30} height={30} className='cursor-pointer' />
         </div>
       </div>
+
       <div
         style={{
-          backgroundColor: "black",
-          height: "1000px",
+          backgroundImage: `url(https://img4.thuthuatphanmem.vn/uploads/2020/06/05/hinh-nen-cong-nghe-don-gian-ma-dep_103228946.jpg)`,
+          height: "1080px",
         }}
-        className='flex flex-col gap-8'
+        className='flex flex-col gap-5'
       >
         <div className='grid grid-cols-5'>
           <div></div>
           <div className='text-[#FFFFFF] flex flex-col col-span-3'>
-            <p className='text-[40px] pt-4 border-b-[2px] border-[#FFFFFF] text-start'>About Me</p>
+            <p className='text-[36px] pt-3 border-b-[2px] border-[#FFFFFF] text-start'>About Me</p>
             <div className='flex flex-col gap-4  text-justify pt-4'>
               <p>
                 My name is Nguyen Duc Viet, and I am currently a student at the Posts and
@@ -101,12 +152,27 @@ const HomePage = () => {
                 industry in Vietnam.
               </p>
             </div>
+            <div className='pt-3'>
+              <Slider {...settings}>
+                {data?.map((el: any, i: number) => (
+                  <div key={i} className='relative'>
+                    <Image
+                      src={el?.banner}
+                      alt=''
+                      width={1000}
+                      height={300}
+                      className='w-full aspect-[1000/300] object-cover rounded-lg'
+                    />
+                  </div>
+                ))}
+              </Slider>
+            </div>
           </div>
           <div></div>
         </div>
         <div className='grid grid-cols-5'>
           <div></div>
-          <div className='text-[#FFFFFF] text-[40px] col-span-3'>
+          <div className='text-[#FFFFFF] text-[36px] col-span-3'>
             <div className='border-b-[2px] border-[#FFFFFF] pb-2 w-[240px]'> My photos</div>
             <div className='flex gap-3 py-6'>
               {data?.map((el: any) => (
@@ -116,7 +182,7 @@ const HomePage = () => {
                     alt=''
                     width={150}
                     height={150}
-                    className='w-full h-full'
+                    className='w-full h-full rounded-md'
                   />
                 </div>
               ))}
@@ -125,7 +191,45 @@ const HomePage = () => {
           <div></div>
         </div>
 
-        <div className='grid grid-cols-5'></div>
+        {/* <div className='grid grid-cols-5'>
+          <div></div>
+          <div className='col-span-3 text-[#FFFFFF]'>
+            <p>Let's get in touch. Send me a message:</p>
+            <div className='flex flex-col gap-2 pt-3'>
+              <Typography variant='body2' fontWeight={500}>
+                Email
+              </Typography>
+              <TextField
+                placeholder='Please fill email'
+                className='bg-white rounded-md'
+                fullWidth
+                size='small'
+                type='email'
+              />
+              <Typography variant='body2' fontWeight={500}>
+                Number phone
+              </Typography>
+              <TextField
+                placeholder='Number'
+                className='bg-white rounded-md'
+                fullWidth
+                size='small'
+              />
+              <Typography variant='body2' fontWeight={500}>
+                Description
+              </Typography>
+              <TextField
+                placeholder='Description'
+                className='bg-white rounded-md'
+                fullWidth
+                size='small'
+              />
+              <button className='px-3 py-2 bg-white text-[#000000] hover:bg-slate-200 w-[60px] rounded-md mt-2'>
+                <SendIcon color='primary' />
+              </button>
+            </div>
+          </div>
+        </div> */}
       </div>
     </div>
   );
